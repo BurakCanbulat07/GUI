5988e04098e38016653951e9b6965a7bfc995c83
BurakCanbulat07 <burakcnblt02@gmail.com>
2021-04-15 17:03:59 +0300

    First Day

--- src/C1_JFrameJPanel.java
+++ src/C1_JFrameJPanel.java
@@ -7,7 +7,7 @@ public class C1_JFrameJPanel {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setSize(200, 200);
-		 
+		  
 		JPanel panel = new JPanel();
 		frame.add(panel);
 		JButton btn = new JButton("Button");
